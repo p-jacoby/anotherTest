a6f5f47b1e31293f932c48f1c6fd82d8c0e004bd
Patrik Jacoby <patrik.jacoby@magicinternet.de>
2014-02-24 15:37:02 +0100

    endlich

--- tagAndStore.sh
+++ tagAndStore.sh
@@ -2,14 +2,11 @@
 
 TAGNAME="RELEASE_CANDIDATE"
 BRANCHNAME=`git branch | grep \* | awk -F" " '{print $2}'`
-echo 'BRANCH: '$BRANCHNAME
 NOW=`date +"%Y-%m-%d %H:%M"`
-FEATURE_BRANCH="\"feature_branch\": \""$BRANCHNAME"\""
-RELEASE_DATE="\"release_date\": $NOW\""
+FEATURE_BRANCH="\"feature_branch\": \"$BRANCHNAME\""
+RELEASE_DATE="\"release_date\": \"$NOW\""
 JSON="{ $FEATURE_BRANCH,"
 JSON=$JSON"$RELEASE_DATE,"
-JSON=$JSON" \"released_to\": \"STAGE\" }"
+JSON=$JSON"\"released_to\": \"STAGE\" }"
 TIMESTAMP=`date +"%s"`
-echo 'TS:'$TIMESTAMP
-echo 'JSON: '$JSON
-curl -X PUT -H "Content-Type: application/json" http://dcp-tools-01:5984/feature-db/$TIMESTAMP --data-urlencode $JSON
+curl -X PUT -H "Content-Type: application/json" -d "$JSON" http://dcp-tools-01:5984/feature-db/$TIMESTAMP
